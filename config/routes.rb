ea7dc41979c712fa744aea618ecbfd5be8dd97e0
Michael Pope <map7@corsairsolutions.com.au>
2016-10-24 18:35:28 +1100

    Add a route to commnad

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'command/cmd'
+  match 'command/:cmd', :to => "command#cmd", via: [:get]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
