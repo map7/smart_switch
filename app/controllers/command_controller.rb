dcc0cd36423478cc45c0bfa1a808f380511f5841
Michael Pope <map7@corsairsolutions.com.au>
2016-10-24 18:52:59 +1100

    Put some switches on the main page.

--- app/controllers/command_controller.rb
+++ app/controllers/command_controller.rb
@@ -2,12 +2,28 @@ class CommandController < ApplicationController
   def cmd
     @result = params[:cmd]
 
-    if @result == "on"
-      %x[/home/map7/hacking/smart_switch/my_code/switch_cli.rb 1]
-    end
+    port = Serial.new '/dev/ttyUSB0', 38400
 
-    if @result == "off"
-      %x[/home/map7/hacking/smart_switch/my_code/switch_cli.rb 2]
+    if @result == 'a'
+      port.write('1')
+      port.write('3')
+      flash[:notice] = "All devices are ON"
+    elsif @result == 'x'
+      port.write('2')
+      port.write('4')
+      flash[:notice] = "All devices are OFF"
+    elsif @result.to_i > 0 and @result.to_i <= 4
+      port.write(@result)
+      case @result.to_i
+      when 1
+        flash[:notice] = "Switch 1 is ON"
+      when 2
+        flash[:notice] = "Switch 1 is OFF"
+      when 3
+        flash[:notice] = "Switch 2 is ON"
+      when 4
+        flash[:notice] = "Switch 2 is OFF"
+      end
     end
   end
 end
