4c13a5f2908a48874864de863adf2a3073ca2580
Michael Pope <map7@corsairsolutions.com.au>
2016-10-24 18:35:37 +1100

    Add controller logic to turn on and off a switch

--- app/controllers/command_controller.rb
+++ app/controllers/command_controller.rb
@@ -1,4 +1,13 @@
 class CommandController < ApplicationController
   def cmd
+    @result = params[:cmd]
+
+    if @result == "on"
+      %x[/home/map7/hacking/smart_switch/my_code/switch_cli.rb 1]
+    end
+
+    if @result == "off"
+      %x[/home/map7/hacking/smart_switch/my_code/switch_cli.rb 2]
+    end
   end
 end
